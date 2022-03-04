8ba3e11cbd8c4b94c09f0d3ed656e137268b6f1c
Anna Mugu <86118286+doublexkid@users.noreply.github.com>
2022-03-04 22:21:29 +0300

    edit solution

--- src/index.js
+++ src/index.js
@@ -38,9 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    function decodeSymbol(arr){
+        let temp = '';
+        let i = 0;
+        if (arr[0] === '*') {
+            return ' ';
+        }
+        do {
+            if( arr[i] + arr[i+1] === '10')
+           {
+            temp = temp + '.';
+           } else if ( arr[i] + arr[i+1] === '11' ) {
+            temp = temp + '-';
+           }
+           i = i + 2;
+        } while ( i < arr.length )
+       return MORSE_TABLE[temp];
+    }
+    let array = expr.split('');
+    let result = '';
+    while ( array.length ) 
+    {
+       result = result + decodeSymbol( array.splice(0,10) ); 
+    }
+ return result;
 }
 
 module.exports = {
     decode
+
 }
